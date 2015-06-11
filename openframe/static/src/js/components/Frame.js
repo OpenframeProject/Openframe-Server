a7033abdab1b9d037c3d32e53904cd577d7b5bc8
Jonathan Wohl <jon@jonwohl.com>
2015-06-10 22:43:40 -0400

    frame preview ratio via settings

--- openframe/static/src/js/components/Frame.js
+++ openframe/static/src/js/components/Frame.js
@@ -21,16 +21,23 @@ var Frame = React.createClass({
 	},
 
 	render: function() {
+		var w_h_ratio = this.state.frame.settings ? this.state.frame.settings.w_h_ratio : 1;
+
+		console.log('w_h_ratio: ', w_h_ratio);
+
 		var divStyle = {
 			backgroundImage: 'url(' + this.state.frame.current_content.url + ')',
-			backgroundSize: 'cover',
-			backgroundRepeat: 'no-repeat',
-			backgroundPosition: 'center center'
+		};
+
+		var whStyle = {
+			paddingBottom: w_h_ratio * 100 + '%'
 		};
 		return (
-			<div className="col-md-12 frame-outer-container">
+			<div className="col-xl-12 frame-outer-container">
 				<button type="button" className="btn btn-primary btn-xs btn-settings hide" data-toggle="modal" data-target="#myModal">S</button>
-	            <div className="frame" style={divStyle} />
+				<div className="frame-inner-container" style={whStyle} >
+	            	<div className="frame" style={divStyle} />
+	            </div>
 	            <div className="frame-name text-center">
 	                <h6>{this.state.frame.name}</h6>
 	            </div>
