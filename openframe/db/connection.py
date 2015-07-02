0fe6c9ac594b3a866e93030a356184d287690dce
Jonathan Wohl <jon@jonwohl.com>
2015-07-01 22:11:40 -0400

    fixed css issue, added auth settings to db

--- openframe/db/connection.py
+++ openframe/db/connection.py
@@ -3,4 +3,7 @@ from pymongo import MongoClient
 
 # global db reference
 mongo_client = MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)
+if settings.MONGO_AUTH:
+    mongo_client.openframe.authenticate(
+        settings.MONGO_USER, settings.MONGO_PASS, mechanism='MONGODB-CR')
 db = mongo_client.openframe
